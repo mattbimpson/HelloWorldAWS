98e6e7eca608eb1a111f8674743260e52ecd0480
matt bimpson <mattbimpson@outlook.com>
2023-08-31 18:46:17 +0100

    lambda changes

--- Lambda/index.ts
+++ Lambda/index.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
 
-export const HelloWorldHandler: Handler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: Handler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   
   return await new Promise((res) => {
     res({
