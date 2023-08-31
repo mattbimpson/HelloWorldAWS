ec0d072fdd5b20e4efc10e9ce7db888d34f07d53
matt bimpson <mattbimpson@outlook.com>
2023-08-31 19:51:14 +0100

    remove comments

--- Terraform/ApiGateway/main.tf
+++ Terraform/ApiGateway/main.tf
@@ -6,35 +6,6 @@ resource "aws_api_gateway_rest_api" "HelloWorldAPI" {
   name = "hello-world-api"
 }
 
-# resource "aws_api_gateway_rest_api" "HelloWorldAPI" {
-#   body = jsonencode({
-#     openapi = "3.0.1"
-#     info = {
-#       title   = "hello world"
-#       version = "1.0"
-#     }
-#     paths = {
-#       "/hello" = {
-#         get = {
-#           x-amazon-apigateway-integration = {
-#             httpMethod           = "GET"
-#             payloadFormatVersion = "1.0"
-#             type                 = "HTTP_PROXY"
-#             uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
-#           }
-#         }
-#       }
-#     }
-#   })
-
-#   description = "Gets the hello world response"
-#   name        = "hello-world"
-
-#   endpoint_configuration {
-#     types = ["REGIONAL"]
-#   }
-# }
-
 resource "aws_api_gateway_resource" "helloResource" {
   rest_api_id = aws_api_gateway_rest_api.HelloWorldAPI.id
   parent_id   = aws_api_gateway_rest_api.HelloWorldAPI.root_resource_id # In this case, the parent id should the gateway root_resource_id.
@@ -56,10 +27,3 @@ resource "aws_api_gateway_integration" "integration" {
   type                    = "AWS_PROXY"
   uri                     = module.lambda.invokeArn
 }
-
-# resource "aws_api_gateway_method_response" "response_200" {
-#   rest_api_id = aws_api_gateway_rest_api.HelloWorldAPI.id
-#   resource_id = aws_api_gateway_resource.helloResource.id
-#   http_method = aws_api_gateway_method.GetHelloWorldMethod.http_method
-#   status_code = "200"
-# }
