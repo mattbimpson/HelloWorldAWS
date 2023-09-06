fe19a3caed77719b76928f28d147543202075acb
matt bimpson <mattbimpson@outlook.com>
2023-09-06 19:15:31 +0100

    add dev deployment

--- Terraform/ApiGateway/main.tf
+++ Terraform/ApiGateway/main.tf
@@ -4,11 +4,12 @@ module "lambda" {
 
 resource "aws_api_gateway_rest_api" "HelloWorldAPI" {
   name = "hello-world-api"
+  # api_key_source = "HEADER"
 }
 
 resource "aws_api_gateway_resource" "helloResource" {
   rest_api_id = aws_api_gateway_rest_api.HelloWorldAPI.id
-  parent_id   = aws_api_gateway_rest_api.HelloWorldAPI.root_resource_id # In this case, the parent id should the gateway root_resource_id.
+  parent_id   = aws_api_gateway_rest_api.HelloWorldAPI.root_resource_id # The parent id should the gateway root_resource_id
   path_part   = "hello"
 }
 
@@ -17,13 +18,22 @@ resource "aws_api_gateway_method" "GetHelloWorldMethod" {
   resource_id   = aws_api_gateway_resource.helloResource.id
   http_method   = "GET"
   authorization = "NONE"
+  # request_parameters = {
+  #   "method.request.header.x-api-key" = true
+  # }
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "Integration" {
   rest_api_id             = aws_api_gateway_rest_api.HelloWorldAPI.id
   resource_id             = aws_api_gateway_resource.helloResource.id
   http_method             = aws_api_gateway_method.GetHelloWorldMethod.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = module.lambda.invokeArn
+}
+
+resource "aws_api_gateway_deployment" "DeploymentDev" {
+  depends_on    = [aws_api_gateway_rest_api.HelloWorldAPI]
+  rest_api_id   = aws_api_gateway_rest_api.HelloWorldAPI.id
+  stage_name    = "dev" 
 }
